c29510dfb5a452f54e6bcb1221aa2e3f4af4d71d
Vinicius Henrique <callamktdigital@gmail.com>
2026-06-24 11:40:11 -0300

    delete

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { auth } from "@/auth"
-import { redirect } from "next/navigation"
-
-export default async function HomePage() {
-  const session = await auth()
-  if (!session) redirect("/auth/signin")
-  if (session.user.isPlatformAdmin) redirect("/admin")
-  redirect("/inbox")
-}
