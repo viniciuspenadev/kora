39c9e8cf2523da0001ceea8919eb85b974f0eb8d
Vinicius Henrique <callamktdigital@gmail.com>
2026-07-28 15:53:39 -0300

    delete

--- src/app/(app)/configuracoes/perfil/page.tsx
+++
@@ -1,9 +0,0 @@
-import { getMyProfile } from "@/lib/actions/profile"
-import { ProfileClient } from "./client"
-
-export const dynamic = "force-dynamic"
-
-export default async function PerfilPage() {
-  const profile = await getMyProfile()
-  return <ProfileClient profile={profile} />
-}
