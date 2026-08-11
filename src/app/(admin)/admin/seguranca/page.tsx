3959c15ea397841183f0a00bf11ada1a47e6fb78
Vinicius Henrique <callamktdigital@gmail.com>
2026-08-11 13:55:30 -0300

    delete

--- src/app/(admin)/admin/seguranca/page.tsx
+++
@@ -1,9 +0,0 @@
-import { getSecurityOverview } from "@/lib/actions/admin-security"
-import { SecurityClient } from "./client"
-
-export const dynamic = "force-dynamic"
-
-export default async function SegurancaPage() {
-  const data = await getSecurityOverview()
-  return <SecurityClient data={data} />
-}
